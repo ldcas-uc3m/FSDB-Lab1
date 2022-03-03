7bf6d2454a78b4de5fdd4d0d96edfd2c4710888b
ldcas-uc3m <78721925+ldcas-uc3m@users.noreply.github.com>
2022-03-03 21:08:04 +0100

    tested sum and added sum

--- migration.sql
+++ migration.sql
@@ -8,18 +8,18 @@ insert into Specialty
   from fsdb.doctors where specialty is not null and desc_specialty is not null;
 
 
--- Insurance_Company -no funciona porque patata (ORA-01722)
+-- Insurance_Company
 insert into Insurance_Company 
   select distinct taxID_insurer, insurer, address_insurer, town_insurer, ZIP_insurer, phone_insurer, email_insurer, web_insurer
   from fsdb.contracts
   where taxID_insurer is not null and insurer is not null and address_insurer is not null and town_insurer is not null and ZIP_insurer is not null and phone_insurer is not null and email_insurer is not null and web_insurer is not null;
 
 
--- Hospital 
+-- Hospital
 INSERT INTO Hospital 
-SELECT DISTINCT hospital, null, address_hospital, address_emergency, town_hospital, country_hospital, ZIP_hospital, phone_hospital
-FROM fsdb.doctors
-WHERE hospital is not null and address_hospital is not null and town_hospital is not null and  country_hospital is not null and ZIP_hospital is not null and phone_hospital is not null;
+  SELECT DISTINCT hospital, null, address_hospital, address_emergency, town_hospital, country_hospital, ZIP_hospital, phone_hospital
+  FROM fsdb.doctors
+  WHERE hospital is not null and address_hospital is not null and town_hospital is not null and  country_hospital is not null and ZIP_hospital is not null and phone_hospital is not null;
 
 
 -- Customer
@@ -40,22 +40,25 @@ where passport = '69100500-J';
 */
 
 
--- Doctor (EN REVISON) ---
+-- Doctor
 insert into Doctor
-select distinct collegiateNum, phoneNum, name, surname1, surname2, passport, hospital
-from fsdb.doctors
-where collegiateNum is not null and phoneNum is not null and name is not null and surname1 is not null and passport is not null;
+  select distinct collegiateNum, phoneNum, name, surname1, surname2, passport
+  from fsdb.doctors
+  where collegiateNum is not null and phoneNum is not null and name is not null and surname1 is not null and passport is not null;
 
 
--- Doctor_Hospital (TODO) ---
-
+-- Doctor_Hospital
+insert into Doctor_Hospital
+  select distinct collegiateNum, hospital
+  from fsdb.doctors
+  where collegiateNum is not null and hospital is not null;
 
 
 -- Hospital_Specialty (REVISAR) --
 INSERT INTO Hospital_Specialty
-select distinct hospital, specialty
-from fsdb.doctors
-where hospital is not null and specialty is not null;
+  select distinct hospital, specialty
+  from fsdb.doctors
+  where hospital is not null and specialty is not null;
 
 
 -- Doctor_Specialty (EN REVISION) ---
@@ -65,7 +68,7 @@ insert into Doctor_Specialty
   where collegiateNum is not null and specialty is not null;
 
 
--- Concert
+-- Concert (REVISAR) --
 insert into Concert
   select distinct taxID_insurer, hospital, start_date, end_date 
   from fsdb.contracts
@@ -74,9 +77,9 @@ insert into Concert
 
 -- Product 
 insert into product
-select distinct product, cif_insurer, version
+  select distinct product, cif_insurer, version
   from fsdb.clients
-  where product is not null and cif_insurer is not null and version is not null ; 
+  where product is not null and cif_insurer is not null and version is not null; 
 
 
 -- Product_Coverages
